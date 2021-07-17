36a29607129f3d830c7482e233d57816592cb58c
Deserie Murembeni <deserie.dev@gmail.com>
2021-07-17 21:17:11 +0200

    Latest working code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from '../images/logo.svg';
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="Around The U.S" />
+      <img className="header__logo" src={logo} alt="Around The World" />
     </header>
   )
 }
