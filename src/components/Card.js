6ab0ba6831f70c2923e2333bae9e706c8c5e02a8
Deserie Murembeni <deserie.dev@gmail.com>
2021-07-27 20:23:18 +0200

    Use context in Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card(props) {
+  //Subscribing to user context
+  const currentUser = React.useContext(CurrentUserContext);
+  // Checking if the current user is the owner of the current card
+  const isOwn = props.card.owner._id === currentUser._id;
+  // Check if the card was liked by the current user
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+
+  // Creating a variable which you'll then set in `className` for the delete button
+  const cardDeleteButtonClassName = (
+    `elements__delete-button ${isOwn ? 'elements__delete-button_visible' : 'elements__delete-button_hidden'}`
+  ); 
+
+  // Create a variable which you then set in `className` for the like button
+  const cardLikeButtonClassName = (
+    `elements__like ${isLiked ? 'elements__like_active' : ''}`
+  )
 
   function handleClick() {
     props.onCardClick(props.card);
@@ -9,11 +26,11 @@ function Card(props) {
   return(
     <li className="elements__item">
       <img className="elements__image" alt={props.name} src={props.link} onClick={handleClick} />
-        <button className="elements__delete-button" type="button"></button>
+        <button className={cardDeleteButtonClassName} type="button"></button>
         <div className="elements__heading">
           <h2 className="elements__title">{props.name}</h2>
           <div className="elements__like-container">
-            <button className="elements__like" type="button" aria-label="like button"></button>
+            <button className={cardLikeButtonClassName} type="button" aria-label="like button"></button>
             <p className="elements__like-counter">{props.likes}</p>
         </div>
       </div>
